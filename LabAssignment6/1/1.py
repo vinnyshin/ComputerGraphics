b42cadecba4f2e7939fff44d917ce1dfd9e9c30e
vinnyshin <tankmom7279@gmail.com>
2021-04-06 10:53:02 +0900

    LabAssignment6

---
+++ LabAssignment6/1/1.py
@@ -0,0 +1,117 @@
+import glfw
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import numpy as np
+
+def myFrustum(left, right, bottom, top, near, far):
+    Mp = np.array([[2 * near / (right - left), 0, (right + left) / (right - left), 0],
+          [0, 2 * near / (top - bottom), (top + bottom) / (top - bottom), 0],
+          [0, 0, -(far + near) / (far - near), (-2 * far * near) / (far - near)],
+          [0, 0, -1, 0]])
+    glMultMatrixf(Mp.T)
+
+def myLookAt(eye, at, up):
+    w_vector = eye - at
+    w_unit = w_vector / np.sqrt(np.dot(w_vector, w_vector))
+    
+    u_vector = np.cross(up, w_unit)
+    u_unit = u_vector / np.sqrt(np.dot(u_vector, u_vector))
+
+    v_unit = np.cross(w_unit, u_unit)
+    
+    Mv = np.identity(4)
+    Mv[:4, :3] = np.array([u_unit, v_unit, w_unit, eye])
+    
+    # Mv
+    # [u1, u2, u3, 0]
+    # [v1, v2, v3, 0]
+    # [w1, w2, w3, 0]
+    # [eye1, eye2, eye3, 1]
+    # We don't have to transpose Mv
+
+    glMultMatrixf(np.linalg.inv(Mv))
+
+def drawUnitCube():
+    glBegin(GL_QUADS)
+    glVertex3f( 0.5, 0.5,-0.5)
+    glVertex3f(-0.5, 0.5,-0.5)
+    glVertex3f(-0.5, 0.5, 0.5)
+    glVertex3f( 0.5, 0.5, 0.5)
+    glVertex3f( 0.5,-0.5, 0.5)
+    glVertex3f(-0.5,-0.5, 0.5)
+    glVertex3f(-0.5,-0.5,-0.5)
+    glVertex3f( 0.5,-0.5,-0.5)
+    glVertex3f( 0.5, 0.5, 0.5)
+    glVertex3f(-0.5, 0.5, 0.5)
+    glVertex3f(-0.5,-0.5, 0.5)
+    glVertex3f( 0.5,-0.5, 0.5)
+    glVertex3f( 0.5,-0.5,-0.5)
+    glVertex3f(-0.5,-0.5,-0.5)
+    glVertex3f(-0.5, 0.5,-0.5)
+    glVertex3f( 0.5, 0.5,-0.5)
+    glVertex3f(-0.5, 0.5, 0.5)
+    glVertex3f(-0.5, 0.5,-0.5)
+    glVertex3f(-0.5,-0.5,-0.5)
+    glVertex3f(-0.5,-0.5, 0.5)
+    glVertex3f( 0.5, 0.5,-0.5)
+    glVertex3f( 0.5, 0.5, 0.5)
+    glVertex3f( 0.5,-0.5, 0.5)
+    glVertex3f( 0.5,-0.5,-0.5)
+    glEnd()
+
+def drawCubeArray():
+    for i in range(5):
+        for j in range(5):
+            for k in range(5):
+                glPushMatrix()
+                glTranslatef(i,j,-k-1)
+                glScalef(.5,.5,.5)
+                drawUnitCube()
+                glPopMatrix()
+
+def drawFrame():
+    glBegin(GL_LINES)
+    glColor3ub(255, 0, 0)
+    glVertex3fv(np.array([0.,0.,0.]))
+    glVertex3fv(np.array([1.,0.,0.]))
+    glColor3ub(0, 255, 0)
+    glVertex3fv(np.array([0.,0.,0.]))
+    glVertex3fv(np.array([0.,1.,0.]))
+    glColor3ub(0, 0, 255)
+    glVertex3fv(np.array([0.,0.,0]))
+    glVertex3fv(np.array([0.,0.,1.]))
+    glEnd()
+
+def render():
+    global gCamAng, gCamHeight
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    glEnable(GL_DEPTH_TEST)
+    glPolygonMode( GL_FRONT_AND_BACK, GL_LINE )
+    glLoadIdentity()
+
+    myFrustum(-1,1, -1,1, 1,10)
+    # glFrustum(-1,1, -1,1, 1,10)
+    myLookAt(np.array([5,3,5]), np.array([1,1,-1]), np.array([0,1,0]))
+    # gluLookAt(5,3,5, 1,1,-1, 0,1,0)
+    drawFrame()
+    glColor3ub(255, 255, 255)
+    drawCubeArray()
+
+def main():
+    if not glfw.init():
+        return
+    window = glfw.create_window(480, 480,'2017029870', None,None)
+    if not window:
+        glfw.terminate()
+        return
+    glfw.make_context_current(window)
+
+    while not glfw.window_should_close(window):
+        glfw.poll_events()
+        render()
+        glfw.swap_buffers(window)
+
+    glfw.terminate()
+
+if __name__ == "__main__":
+    main()
